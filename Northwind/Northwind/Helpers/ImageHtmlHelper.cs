176ba9e6836ab5457e5b284e072d1ecd309b47c9
Aliaksei Lazouski@epam.com <Aliaksei_Lazouski@epam.com>
2020-04-08 17:04:04 +0300

    stage 3, part 3 html and tag helpers

---
+++ Northwind/Northwind/Helpers/ImageHtmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace Northwind.Helpers
+{
+    public static class ImageHtmlHelper
+    {
+        public static HtmlString NorthwindImageLink(this IHtmlHelper helper, string imageId, string text)
+        {
+            return new HtmlString($"<a href='/images/{imageId}'>{text}</a>");
+        }
+    }
+}
---
+++ Northwind/Northwind/Helpers/ImageTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class ImageTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"/images/{context.AllAttributes["northwind-id"].Value}");
+        }
+    }
+}
