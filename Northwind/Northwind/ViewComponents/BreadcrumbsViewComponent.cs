c6101d49becd1d8df38ad09ddf24f1b1c8dc1494
Aliaksei Lazouski@epam.com <Aliaksei_Lazouski@epam.com>
2020-04-08 16:08:02 +0300

    stage 3, part 2: breadcrumbs view component

---
+++ Northwind/Northwind/ViewComponents/BreadcrumbsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.ViewComponents
+{
+    public class BreadcrumbsViewComponent: ViewComponent
+    {
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var controller = ViewContext.RouteData.Values["controller"].ToString();
+            var action = ViewContext.RouteData.Values["action"].ToString();
+            var breadcrumbs = new List<BreadcrumbViewModel>();
+            breadcrumbs.Add(new BreadcrumbViewModel() { DisplayName = "Home", Url = "/" });
+            breadcrumbs.Add(new BreadcrumbViewModel() { DisplayName = controller, Url = $"/{controller}" });
+
+            if (!action.Equals("Index"))
+            {
+                breadcrumbs.Add(new BreadcrumbViewModel() { DisplayName = action });
+            }
+            return Task.FromResult<IViewComponentResult>(View(breadcrumbs));
+        }
+    }
+}
